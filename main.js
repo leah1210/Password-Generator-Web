c5b52e62c8684d069fc1317923d9c84bd9d2a09e
Leah <leaharonhime@gmail.com>
2021-05-19 20:08:41 -0400

    second commit-clean up code

--- main.js
+++ main.js
@@ -1,50 +1,26 @@
-// const myLine = document.createElement('p')
-// const myText = document.createTextNode('I am from javascript')
 
-// myLine.appendChild(myText)
-// document.body.appendChild(myText)
-
-// const theElement = document.querySelector('#this-one')
-// setTimeout(() => {
-//   theElement.innerText = ('These words are from javascript');
-// }, 3000);
-
-// const anotherElement = document.querySelector('#second-one')
-
-// const button = document.getElementById('button')
-// button.onclick = () => {
-//   anotherElement.innerText = 'I have changed'
-// }
 
 //************************* whatever happens on the click needs tobe in onclick.  everything else runs 1 time when the page is reloaded *************
 
 //------------------- justLettersPassword function --------------------//
 
 const justLetters = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890'
-const justLettersLength = justLetters.length
-const randomCharacterLetters = () => {
-  const indexLetters = getRandomIntInclusive(0, justLettersLength - 1)
-  return justLetters[indexLetters]
-}
-const justLettersPassword = x => {
-  return buildArray(x).map(randomCharacterLetters).join('')
-}
-console.log(justLettersPassword(10));
+
+const characters = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*(){}?/.,<>:"][]'
 
 //------------------- password function --------------------//
 
-const characters = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*(){}?/.,<>:"][]'
-const charactersLength = characters.length
-
-const randomCharacter = () => {
-  const index = getRandomIntInclusive(0, charactersLength - 1)
-  return characters[index]
+const randomCharacter = (characterSet) => {
+  const index = getRandomIntInclusive(0, characterSet.length - 1)
+  return characterSet[index]
 }
 
-const password = x => {
-  return buildArray(x).map(randomCharacter).join('')
+const makePassword = (length, characterSet) => {
+  return buildArray(length).map(() => randomCharacter(characterSet)).join('')
 }
-console.log(password(5));
+
+console.log(makePassword(10, characters));
+
 
 //----------------------------- onclick ---------------------------------//
 
@@ -52,19 +28,9 @@ const button = document.getElementById('create')
 
 button.onclick = () => {
   const x = document.getElementById('characters')
-  //console.log(x.value);
   const stringToNum = Number(x.value)
-  //console.log(stringToNum);
-  //console.log(typeof stringToNum);
-  console.log(password(stringToNum));
-  const randomPassword = password(stringToNum)
-  const randomPassword2 = password(stringToNum)
-  const randomPassword3 = password(stringToNum)
-  //-------- random letter passwords:
-  const randomLettersPassword = justLettersPassword(stringToNum)
-  const randomLettersPassword2 = justLettersPassword(stringToNum)
-  const randomLettersPassword3 = justLettersPassword(stringToNum)
-  //-------- random lowercase passwords:
+
+
 
   //-------- This is what is printing on the web page ----------//
   const cbSpecial = document.getElementById('special');
@@ -74,65 +40,42 @@ button.onclick = () => {
   if (cbSpecial.checked === true) {
     const theElement = document.querySelector('#random-password-1')
     setTimeout(() => {
-      theElement.innerText = (randomPassword);
+      theElement.innerText = (makePassword(stringToNum, characters));
     }, 200);
 
     const theElement2 = document.querySelector('#random-password-2')
     setTimeout(() => {
-      theElement2.innerText = (randomPassword2);
+      theElement2.innerText = (makePassword(stringToNum, characters));
     }, 200);
 
     const theElement3 = document.querySelector('#random-password-3')
     setTimeout(() => {
-      theElement3.innerText = (randomPassword3);
+      theElement3.innerText = (makePassword(stringToNum, characters));
     }, 200);
 
   } else if (cbLetters.checked === true) {
     const theElement4 = document.querySelector('#random-password-1')
     setTimeout(() => {
-      theElement4.innerText = (randomLettersPassword);
+      theElement4.innerText = (makePassword(stringToNum, justLetters));
     }, 200);
 
     const theElement5 = document.querySelector('#random-password-2')
     setTimeout(() => {
-      theElement5.innerText = (randomLettersPassword2);
+      theElement5.innerText = (makePassword(stringToNum, justLetters));
     }, 200);
 
     const theElement6 = document.querySelector('#random-password-3')
     setTimeout(() => {
-      theElement6.innerText = (randomLettersPassword3);
+      theElement6.innerText = (makePassword(stringToNum, justLetters));
     }, 200);
   } else {
     'not applicable'
   }
-  //--------------------- Check boxes ---------------------------------//
-  // const cbSpecial = document.getElementById('special');
-  // const cbLetters = document.getElementById('letters');
-  // const cbLowercase = document.getElementById('lowercase');
-
-  if (cbSpecial.checked === true) {
-    console.log(randomPassword);
-  } else if (cbLetters.checked === true) {
-    console.log(randomLettersPassword);
-  } else if (cbLowercase.checked === true) {
-    console.log(theElement3);
-  } else {
-    console.log('not applicable');
-  }
-
-  // const y = document.getElementById('special')
-  // console.log(y);
-  // const z = document.getElementById('letters')
-
-
-  // if (y.checked === true) {
-  //   console.log(randomPassword);
-  // } else if (z.checked === true) {
-  //   console.log(justLettersPassword);
-  // }
 
   //--------------------- Copy to Clipboard ---------------------------------//
-
+  // css = display none
+  //js = display inline block
+  //display value none
   //   const copyText = () => {
   //     /* Get the text field */
   //     const copyText = document.getElementById("password");
